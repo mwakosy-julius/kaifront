d893e49de6ff33017beb7be1229ae3b870f7843d
ibrahim9757 <147980611+ibrahim9757@users.noreply.github.com>
2025-06-22 09:47:50 +0530

    Add files via upload

--- main.tsx
+++ main.tsx
@@ -8,8 +8,8 @@ import App from "./App.tsx";
 // Styles
 import "./index.css";
 
-const clientId =
-  "133426663689-2i05ddo0v7kcqikp29cgka2h1ob25cc8.apps.googleusercontent.com";
+// Define your Google OAuth client ID here
+const clientId = "133426663689-2i05ddo0v7kcqikp29cgka2h1ob25cc8.apps.googleusercontent.com";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
