73e72d589540efec1c9b385080e43b834125b87e
tacherasasi <tacherasasi@gmail.com>
2025-07-31 09:49:55 +0300

    Ellie: simple error handling in the main.tsx for the clerk comp

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,12 @@ import App from "./App.tsx";
 // Styles
 import "./index.css";
 
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Add then Clerk Publishable Key to the .env file')
+}
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
