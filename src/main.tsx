1fe62590c00910eb77da6b7bff0502c358c3c5e0
tacherasasi <tacherasasi@gmail.com>
2025-07-31 17:11:58 +0300

    removed the check for the publishable key that throw an error if not available

--- src/main.tsx
+++ src/main.tsx
@@ -6,9 +6,9 @@ import "./index.css";
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
-if (!PUBLISHABLE_KEY) {
-  throw new Error('Add your Clerk Publishable Key to the .env file')
-}
+// if (!PUBLISHABLE_KEY) {
+//   throw new Error('Add your Clerk Publishable Key to the .env file')
+// }
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
