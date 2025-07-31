a9f7e08cec905a299a5dd2323d66a7917df1aa75
tacherasasi <tacherasasi@gmail.com>
2025-07-31 09:50:41 +0300

    Ellie: code format and more

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
-// Local imports
+import { ClerkProvider } from '@clerk/clerk-react'
 import App from "./App.tsx";
-
-// Styles
 import "./index.css";
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
 if (!PUBLISHABLE_KEY) {
-  throw new Error('Add then Clerk Publishable Key to the .env file')
+  throw new Error('Add your Clerk Publishable Key to the .env file')
 }
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>
-);
-
-
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <App />
+    </ClerkProvider>
+  </StrictMode>,
+)
